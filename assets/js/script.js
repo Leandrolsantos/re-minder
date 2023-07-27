d13ec790392480ebfdcc74fbb73e990da23d3406
Leandro Leme dos Santos <leandrolemesantos@gmail.com>
2023-07-27 20:09:41 -0300

    206º dia de CTD - Criando botão add input/texto.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,12 @@ const chk = document.getElementById('chk');
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark');
 })
+
+function criarInputTexto() {
+    
+    var novoInput = document.createElement("input");
+    novoInput.type = "text";
+    
+    var container = document.getElementById("inputsContainer");
+    container.appendChild(novoInput);
+}
